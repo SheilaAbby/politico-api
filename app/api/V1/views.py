f196ab80e1c1c18219abec2823b8630e7dde383d
SheilaAbby <sheilaabigael2@gmail.com>
2019-02-09 22:08:16 +0300

    [Feature #163749786] added party id data

--- app/api/V1/views.py
+++ app/api/V1/views.py
@@ -4,23 +4,33 @@ from app.api.V1.models import PartyModel
 
 # make a Blueprint route called app_route
 app_route = Blueprint('politico-v1', __name__)
+party = PartyModel()  # creates an instance of the PartyModel class
 
 
 @app_route.route('/parties', methods=['POST'])
-def post_party():  # method handles posting of data
+def post_party():
+    """"This endpoint enables admin user
+      - to create political party"""
+
     data = request.get_json()
+    name = data.get('name')
+    hdaddress = data.get('hqAddress')
+    logourl = data.get('logoUrl')
 
-    name = data["name"]  # from the party_data get name value
-    hqaddress = data["hqAddress"]
-    logourl = data["logoUrl"]  # from the party_data get logourl value
+    if name and hdaddress and logourl:  # when both fields are filled return a 201, success code
+        new_party_data = party.creating_party(name, hdaddress, logourl)
+        return make_response(jsonify({
+            "status": 201,
+            "data": [{
+              "id": new_party_data["id"],
+              "name": new_party_data["name"]
+            }],
+            })), 201
 
-    party = PartyModel(name=name, logourl=logourl, hqaddress=hqaddress)
+    else:          # else if fields are blank return a 400, bad request
+        return make_response(jsonify({
+            "status": 400,
+            "data": [{
+              "message": "some required fields missing"}]})), 400
 
-    party.creating_party()
-
-    return make_response(jsonify({
-        "status": 201,
-        "message": "Political Party created successfully",
-        "data": data
-    })), 201
 
